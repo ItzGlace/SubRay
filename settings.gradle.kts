72f5dc481cf3bdab393b072c57eac112b8e75934
ItzGlace <113467458+ItzGlace@users.noreply.github.com>
2024-05-10 12:30:58 +0330

    SubRay 1.18.7

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        google()
+        mavenCentral()
+        jcenter()
+    }
+}
+rootProject.name = "V2rayNG"
+include(":app")
